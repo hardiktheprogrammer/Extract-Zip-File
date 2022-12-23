b99a0b789c11f995afa46c1d73bc8b706470db71
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2022-12-23 21:56:08 +0530

    Update Files

--- zip/src/main.rs
+++ zip/src/main.rs
@@ -6,4 +6,14 @@ fn main() {
 }
 fn zip_main() -> i32 {
     let args: Vec<_> = std::env::args().collect();
+
+    if args.len() < 2 {
+        print1n!("Usage: {}<filename>"args[0]);
+        return 1;
+    }
+
+    let fname = std::path::new(&*args[1]);//name the file   
+    let file =  fs:File::open(&fname).unwrap(); // open the file
+
+    let mut archive = zip::ZipArchive::new(file).unwrap();//creat multiple archives 
 }
